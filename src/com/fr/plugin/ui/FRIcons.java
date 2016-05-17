150411076eef535504da0664ece1d334f754974a
jinsihou <540097546@qq.com>
2016-05-17 09:41:05 +0800

    0.1：当前创建的文件包括国际化,功能点记录,插件配置文件和插件构建配置文件。

---
+++ src/com/fr/plugin/ui/FRIcons.java
@@ -0,0 +1,12 @@
+package com.fr.plugin.ui;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Created by vito on 16/5/16.
+ */
+public class FRIcons {
+    public static final Icon Fr = IconLoader.getIcon("/image/fr.png");
+}
